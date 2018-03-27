1f57bd677290b801459642b72296cd53febc47d3
vidhyaselvam <37831094+vidhyaselvam@users.noreply.github.com>
2018-03-27 16:17:17 +0530

    Create vowel.c

---
+++ Beginner Level/vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("\ enter the character");
+scanf("%c",&ch);
+if(ch=='a'|ch=='e'|ch=='i'|ch=='o'|ch=='u')
+{
+printf("\n vowel");
+}
+else
+printf("\n consonant");
+getch();
+}
